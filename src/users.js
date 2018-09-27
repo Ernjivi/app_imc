56054c0cc50af13d3f14ebaf805341a1949702ba
Ernesto Jiménez Villaseñor <ernesto@bedu.org>
2018-09-26 21:55:31 -0500

    Example 2 finished

--- src/users.js
+++ src/users.js
@@ -1,22 +1,33 @@
+// Cache de DOM
+
+var firstName = document.querySelector('#firstName');
+var lastName = document.querySelector('#lastName');
+var birthDate = document.querySelector('#birthDate');
+var submit = document.querySelector('#submit');
+var lista = document.querySelector('#lista');
+var data = [];
 
 
-// Guardar datos de usuario en un array
-
-/*
-    User
-
-    {
-        firstName: 'string',
-        lastName: 'string',
-        birthDate: Date
+var processForm = () => {
+    return {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        birthDate: Date.parse(birthDate.value),
     }
-*/
+}
 
-// Cache DOM
+var render = () => {
+    lista.innerHTML = '';
+    data.forEach((user) => {
+        let item = document.createElement('li');
+        let itemText = document.createTextNode(
+            `${user.firstName} ${user.lastName} ${JSON.stringify(user.birthDate)}`);
+        item.appendChild(itemText);
+        lista.appendChild(item)
+    })
+}
 
-// Funciones necesarias.
-// e.g.
-// processForm
-// render
-
-// Registro de eventos
+submit.addEventListener('click', (evt) => {
+    data.push(processForm());
+    render();
+})
