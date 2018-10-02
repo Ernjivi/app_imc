2bd3a2ec12d540a7a9c36ed136db137a92c86f6d
Ernesto Jiménez Villaseñor <ernesto@bedu.org>
2018-10-01 21:54:31 -0500

    Ejercicio async

--- src/users.js
+++ src/users.js
@@ -7,12 +7,19 @@ var submit = document.querySelector('#submit');
 var lista = document.querySelector('#lista');
 var data = [];
 
+var init = () => {
+    fetch("http://localhost:3000/users")
+        .then((users) => users.json())
+        .then((jsonData) => {
+            data = jsonData;
+            render();
+        })
+}
 
 var processForm = () => {
     return {
-        firstName: firstName.value,
-        lastName: lastName.value,
-        birthDate: Date.parse(birthDate.value),
+        first_name: firstName.value,
+        last_name: lastName.value
     }
 }
 
@@ -21,13 +28,36 @@ var render = () => {
     data.forEach((user) => {
         let item = document.createElement('li');
         let itemText = document.createTextNode(
-            `${user.firstName} ${user.lastName} ${JSON.stringify(user.birthDate)}`);
+            `${user.first_name} ${user.last_name}`);
         item.appendChild(itemText);
         lista.appendChild(item)
     })
 }
 
+var createUser = () => {
+
+    console.log(processForm());
+    fetch("http://localhost:3000/users", {
+        method: 'POST',
+        body: JSON.stringify(processForm()),
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            init();
+        }
+        return response.json()
+    })
+    .then(responseJson => console.log(responseJson))
+    .catch(e => console.log(e));
+}
+
 submit.addEventListener('click', (evt) => {
-    data.push(processForm());
+    // data.push(processForm());
+    createUser();
     render();
 })
+
+init();
