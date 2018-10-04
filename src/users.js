b676adb3ff351f6a3956f73612f75a7d8adc220c
Ernesto Jiménez Villaseñor <ernesto@bedu.org>
2018-10-03 20:25:04 -0500

    start example

--- src/users.js
+++ src/users.js
@@ -1,63 +1,31 @@
-// Cache de DOM
 
-var firstName = document.querySelector('#firstName');
-var lastName = document.querySelector('#lastName');
-var birthDate = document.querySelector('#birthDate');
-var submit = document.querySelector('#submit');
-var lista = document.querySelector('#lista');
-var data = [];
 
-var init = () => {
-    fetch("http://localhost:3000/users")
-        .then((users) => users.json())
-        .then((jsonData) => {
-            data = jsonData;
-            render();
-        })
-}
+export default class UserList{
 
-var processForm = () => {
-    return {
-        first_name: firstName.value,
-        last_name: lastName.value
+    constructor(selector){
+        this.el = document.querySelector(selector);
+        this.cacheDOM();
+        this.fetchUsers()
+            .then(response => response.json())
+            .then(data => {
+                this.data = data;
+            });
+    }
+
+    cacheDOM(){
+        this.inputFirstName = this.el.querySelector('#firstName');
+        this.inputLastName = this.el.querySelector('#lastName');
+        this.submitButton = this.el.querySelector('#submit');
+        this.list = this.el.querySelector('#list');
+    }
+
+    createUser(){
+        // 
+    }
+
+    fetchUsers(){
+        return fetch('http://localhost:3000/users')
     }
 }
 
-var render = () => {
-    lista.innerHTML = '';
-    data.forEach((user) => {
-        let item = document.createElement('li');
-        let itemText = document.createTextNode(
-            `${user.first_name} ${user.last_name}`);
-        item.appendChild(itemText);
-        lista.appendChild(item)
-    })
-}
-
-var createUser = () => {
-
-    console.log(processForm());
-    fetch("http://localhost:3000/users", {
-        method: 'POST',
-        body: JSON.stringify(processForm()),
-        headers:{
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => {
-        if (response.ok) {
-            init();
-        }
-        return response.json()
-    })
-    .then(responseJson => console.log(responseJson))
-    .catch(e => console.log(e));
-}
-
-submit.addEventListener('click', (evt) => {
-    // data.push(processForm());
-    createUser();
-    render();
-})
-
-init();
+var userList = new UserList('#userList');
